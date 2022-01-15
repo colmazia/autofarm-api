a8effdde5a32354736024fefeb7477e6f4c51c4f
colmazia <ongart.pisan@gmail.com>
2022-01-15 18:11:18 +0700

    add getPoolsInfo fn and local var that give autofarm pools info

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+let pools: Array<any> = []
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test/eth')
+  getTest(): any {
+    return this.appService.getTestEth();
+  }
+
+  @Get('autofarm/cache/update')
+  async getPoolsInfo(): Promise<any> {
+    if (pools.length === 0) {
+      pools = await this.appService.getPoolsInfo();
+      return {
+        pools: pools
+      }
+    }
+    return {
+      pools: pools
+    }
   }
 }
